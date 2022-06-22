07ef958c07a289c6ffe1ae9e7919fdc366d8a0c2
dPenedo <dpenedo@tutanota.com>
2022-06-22 17:32:27 -0300

    Cambios en el README, mapa visual y avance del tutorial

--- pages/pages_project/settings.py
+++ pages/pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=!0s%#je=oe%yopv0byyl4d=z5os!or%!z&x3evo)ds)7ipt*h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
